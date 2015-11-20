ddb72fcaf6f32022cc83f092777e1db91ae9e1ac
Robin Hermans <r.hermans@maxxton.com>
2015-11-20 10:34:35 +0100

    Added support for publishing your project with definition files.

--- generators/app/templates/project/_gulpfile.js
+++ generators/app/templates/project/_gulpfile.js
@@ -6,21 +6,27 @@ var sourcemaps = require('gulp-sourcemaps');
 var liveServer = require('gulp-live-server');
 var embedTemplates = require('gulp-angular-embed-templates');
 var sass = require('gulp-sass');
+var merge = require('merge2');
 var clean = require('gulp-clean');
 
 var tsconfig = require('./tsconfig.json');
 
 gulp.task('default', ['compile', 'run']);
 
-gulp.task('compile', ['compile-sass', 'compile-typescript', 'compile-assets', 'compile-html'], function(){
+gulp.task('compile', ['compile-sass', 'compile-typescript', 'compile-assets', 'compile-html', 'compile-package'], function(){
   gulp.watch(['src/**/*.ts', 'src/**/*.html'], ['compile-typescript']);
   gulp.watch('src/assets/scss/**/*.scss', ['compile-sass']);
   gulp.watch(['src/assets/fonts/**/**', 'src/assets/images/**/**'], ['compile-assets']);
   gulp.watch('src/index.html', ['compile-html']);
+  gulp.watch(['package.json', 'LICENSE', 'README.md'], ['compile-package'])
 });
 
 gulp.task('compile-typescript', [], function(){
-  gulp.src('src/**/*.ts').pipe(sourcemaps.init()).pipe(typescript(tsconfig)).js.pipe(embedTemplates()).pipe(sourcemaps.write('.', {sourceRoot: '/src'})).pipe(gulp.dest('dist'));
+  var result = gulp.src('src/**/*.ts').pipe(sourcemaps.init()).pipe(typescript(tsconfig));
+  return merge([
+    result.dts.pipe(gulp.dest('dist')),
+    result.js.pipe(embedTemplates()).pipe(sourcemaps.write('.', {sourceRoot: '/src'})).pipe(gulp.dest('dist'))
+  ]);
 });
 
 gulp.task('compile-sass', [], function(){
@@ -34,6 +40,10 @@ gulp.task('compile-assets', [], function() {
 
 gulp.task('compile-html', [], function(){
   return gulp.src('src/index.html').pipe(gulp.dest('dist'));
+});
+
+gulp.task('compile-package', [], function() {
+  return gulp.src(['package.json', 'LICENSE', 'README.md']).pipe(gulp.dest('dist'));
 });
 
 gulp.task('run', [], function(){
